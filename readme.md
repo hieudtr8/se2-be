87f70b53636f742597a656c28c97183f1309eaaf
lamlam-l <lam.2002.2015@gmail.com>
2023-02-19 11:27:29 +0700

    init project

---
+++ readme.md
@@ -0,0 +1,1 @@
+# SE2 ecommerce project
---
+++ src/test/java/com/se2_sever/Se2SeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se2_sever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2SeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
