82c64a7055645e0a48bed3caef306f7ef5c2d2e9
lamlam-l <lam.2002.2015@gmail.com>
2023-05-06 21:31:01 +0700

    get product by ids post

--- src/main/java/main/controller/product/ListProductByIds.java
+++ src/main/java/main/controller/product/ListProductByIds.java
@@ -5,10 +5,7 @@ import main.service.product.SearchProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Comparator;
 import java.util.List;
@@ -24,7 +21,7 @@ public class ListProductByIds {
         public String[] ids;
     }
 
-    @GetMapping(value = "/product/ids")
+    @PostMapping(value = "/product/ids")
     public ResponseEntity<Response<?>> getProductByIds(
             @RequestBody IdsRequestBody body
     ) {
